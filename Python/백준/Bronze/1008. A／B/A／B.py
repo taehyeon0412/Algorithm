e249696b327850c8bbdd745923ae6e234eb61f61
TaeHyeon <71374539+taehyeon0412@users.noreply.github.com>
2024-07-17 19:44:13 +0900

    [Bronze V] Title: A/B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1008. A／B/A／B.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+# input.split = 사용자가 입력한 문자열을 공백을 기준으로 분리
+# map을 사용하여 분리된 리스트를 int로 바꿔줌
+
+if A > 0 and B < 10:
+    print(A/B)
+else:
+    print("A는 0보다 커야되고 B는 10보다 작아야 됩니다.")
