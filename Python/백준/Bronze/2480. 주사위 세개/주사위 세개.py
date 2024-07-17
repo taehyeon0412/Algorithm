e5bee79e5ae5b7b07e7ae06d20403a167ad25726
TaeHyeon <71374539+taehyeon0412@users.noreply.github.com>
2024-07-17 23:58:21 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+a, b, c = map(int, input().split())
+
+maxInt = max(a, b, c)
+
+if a == b == c:
+    print(10000 + a * 1000)
+elif a == b or b == c or a == c:
+    if a == b:
+        print(1000 + a * 100)
+    elif b == c:
+        print(1000 + b * 100)
+    elif a == c:
+        print(1000 + a * 100)
+
+else:
+    print(maxInt * 100)
