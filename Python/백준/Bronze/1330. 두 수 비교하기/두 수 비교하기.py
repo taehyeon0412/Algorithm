fd57bae4fae8ff5341ef65a86ff614949a21a637
TaeHyeon <71374539+taehyeon0412@users.noreply.github.com>
2024-07-17 13:48:40 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,8 @@
+a,b = map(int,input().split()) #input.split = 사용자가 입력한 문자열을 공백을 기준으로 분리
+                               # map을 사용하여 분리된 리스트를 int로 바꿔줌
+if a>b: 
+    print(">")
+elif a<b : 
+    print("<")
+else:
+    print("==") 
