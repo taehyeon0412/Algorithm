3746e98e077ea6027d49035e74ae48e9b0baa27c
TaeHyeon <71374539+taehyeon0412@users.noreply.github.com>
2024-07-29 12:18:05 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2336 ms, Memory: 34016 KB -BaekjoonHub

---
+++ Python/백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,28 @@
+# 요세푸스 문제
+# 원형 리스트에서 매 K번째 사람을 제거 // 큐를 이용해서 풀이
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+# N, K 입력 받기
+N, K = map(int, input().strip().split())
+
+# 큐 초기값
+queue = deque(range(1, N + 1))
+
+# 결과 초기값
+answer = []
+
+# 큐에서 k번째 제거
+while queue:
+    # K-1명 큐의 뒤로 이동
+    for _ in range(K - 1):
+        queue.append(queue.popleft())
+
+    # k 제거하고 결과에 추가
+    answer.append(queue.popleft())
+
+# 출력
+print("<" + ", ".join(map(str, answer)) + ">")
